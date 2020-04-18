4024bf38e4fd21f9ed7ad7682bc5eae15d47237b
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2020-04-18 21:35:44 +0300

    Added preset colors

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const secure = require("express-force-https");
+const path = require("path");
+
+const app = express();
+
+//  Force HTTPS Middleware
+app.use(secure);
+
+app.use(express.static(path.join(__dirname, "Frontend/build")));
+
+//  Production mode
+if (process.env.NODE_ENV === "production") {
+  //  Static folder
+  app.use(express.static(path.join(__dirname, "Frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendfile(path.join((__dirname = "Frontend/build/index.html")))
+  );
+}
+
+//  Start server
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`server listening on port: ${port}`));

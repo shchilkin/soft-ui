60983c7ac5d95832dc8e17698edd8b78a711871e
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2020-04-20 16:01:44 +0300

    Added build for gituhub pages

---
+++ Frontend/build/precache-manifest.39af3c137559f8552e05fd0895424111.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5259981e7ba21d60c7fcf3de1969a97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abd5f88981a1ed2e373a",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "5e12810f6bff237359c3",
+    "url": "/static/css/main.751a904b.chunk.css"
+  },
+  {
+    "revision": "abd5f88981a1ed2e373a",
+    "url": "/static/js/2.bafa4139.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.bafa4139.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e12810f6bff237359c3",
+    "url": "/static/js/main.c5c928ee.chunk.js"
+  },
+  {
+    "revision": "822ef768d331b2ac3a5c",
+    "url": "/static/js/runtime-main.d030b3fa.js"
+  }
+]);

de5584bc21c52a7f0ae48d648aab130bf2491ddf
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2020-05-19 21:28:32 +0300

    Changed how shadows will calculate in button component

--- Frontend/src/Components/Generator/Generator_Components/Layout/Button.js
+++ Frontend/src/Components/Generator/Generator_Components/Layout/Button.js
@@ -1,9 +1,11 @@
 import React, {useContext} from "react";
 import {DefaultButton} from "../../../../StyledComponents";
 import ThemeContext from '../../../../contexts/theme/ThemeContext'
+import {calculateTintAndShades} from '../../../../Functions'
 
 
 const Button = ({children,
+                    sameShadowColor = false,
                     onClick,
                     style,
                     background,
@@ -23,11 +25,13 @@ const Button = ({children,
 
     const {Red, Green, Blue} = colorRGB;
 
-    const lighterShadows = shadows.ligherShadowArray;
-    const darkerShadows = shadows.darkerShadowArray;
+    // const lighterShadows = shadows.ligherShadowArray;
+    // const darkerShadows = shadows.darkerShadowArray;
     const mainColor = `rgb(${Red}, ${Green}, ${Blue})`;
-    const lighterShadow = `rgb(${lighterShadows[0]}, ${lighterShadows[1]}, ${lighterShadows[2]})`;
-    const darkerShadow = `rgb(${darkerShadows[0]}, ${darkerShadows[1]}, ${darkerShadows[2]})`;
+    //Calculates shadows of chosen color if sameShadowColor prop is true,
+    // otherwise make shadows for white (#FFF)background
+    const lighterShadow = sameShadowColor ? calculateTintAndShades(Red,Green,Blue,105) : '#FFF';
+    const darkerShadow = sameShadowColor ? calculateTintAndShades(Red,Green,Blue,85) : '#D9D9D9';
     console.log('Background', background)
 
     return (

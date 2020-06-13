bad01c140126afc3a334e6a2d62ac6a7ea5f6628
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2020-06-13 19:16:29 +0300

    Minor updates to the ColorButton component

--- Frontend/src/Components/Updated/ColorButton.js
+++ Frontend/src/Components/Updated/ColorButton.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, {useContext} from "react";
 import styled from "styled-components";
 import {ComponentShadows} from "../../Classes";
+import ThemeContext from "../../contexts/theme/ThemeContext";
 
 const ColorButton = ({color, onClick}) => {
 
     const componentShadows = new ComponentShadows();
 
+    const {colorHEX} = useContext(ThemeContext);
+
+    console.log('COLOR BUTTON', colorHEX, color.replace('#',''))
+
     const {dark, light} = componentShadows.getShadows(color,true,undefined);
 
+    console.log(color.replace('#', '') === colorHEX)
     return(
         <StyledColorButton
+            isActive={color.replace('#', '') === colorHEX}
             onClick={onClick}
             color={color}
             dark={dark}
@@ -28,10 +35,12 @@ const StyledColorButton = styled.button`
     background-color: ${props => props.color};
     width: 38px;
     height: 38px;
-    box-shadow: 3px 3px 4px 0 #D9D9D9,
-               -3px -3px 5px 0  #FFF;            
+    box-shadow: ${props => props.isActive ? `inset 3px 3px 4px 0 ${props.dark},
+                 inset -3px -3px 5px 0  ${props.light};` : '3px 3px 4px 0 #D9D9D9, -3px -3px 5px 0  #FFF;'}           
     :active {
         box-shadow: inset 3px 3px 4px 0 ${props => props.dark},
                  inset -3px -3px 5px 0  ${props => props.light};
     }
 `;
+
+

2568fe9fdd5afd6658993dc7f7760db9f62e58b6
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2021-02-18 16:02:04 +0200

    Version 2.0 Setup

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+     <Head>
+        <title>Soft UI</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  )
+}

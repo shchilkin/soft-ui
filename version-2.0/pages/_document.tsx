5ce6356474efebd6dc2c781207b407ec1bfbbd76
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2021-03-18 20:03:33 +0200

    Created static folder to fix favicon

--- version-2.0/pages/_document.tsx
+++ version-2.0/pages/_document.tsx
@@ -13,12 +13,11 @@ class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
-
-          <link rel='apple-touch-icon' sizes='180x180' href='/public/apple-touch-icon.png' />
-          <link rel='icon' type='image/png' sizes='32x32' href='/public/favicon-32x32.png' />
-          <link rel='icon' type='image/png' sizes='16x16' href='/public/favicon-16x16.png' />
-          <link rel='manifest' href='/public/site.webmanifest' />
-          <link rel='mask-icon' href='/public/safari-pinned-tab.svg' color='#5bbad5' />
+          <link rel='apple-touch-icon' sizes='180x180' href='/static/apple-touch-icon.png' />
+          <link rel='icon' type='image/png' sizes='32x32' href='/static/favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/static/favicon-16x16.png' />
+          <link rel='manifest' href='/static/site.webmanifest' />
+          <link rel='mask-icon' href='/static/safari-pinned-tab.svg' color='#5bbad5' />
           <meta name='msapplication-TileColor' content='#da532c' />
           <meta name='theme-color' content='#ffffff' />
           <link rel='stylesheet' href='https://use.typekit.net/eqx2cgo.css' />

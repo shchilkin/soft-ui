9df15aeed7119eabe8d14efa4ed8682866f20ac5
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2021-03-23 17:17:17 +0200

    Added documentation for util functions

--- version-2.0/utils/getColorDifference.ts
+++ version-2.0/utils/getColorDifference.ts
@@ -1,10 +1,22 @@
 import { Percent, RgbColor, RgbValue } from '../shared';
 
-//TODO: Add documentation
-export default function getColorDifference(
-  color: RgbColor,
-  percent: Percent,
-  colorDifferenceUtility: (color: RgbValue, percent: Percent) => RgbValue): RgbColor {
+interface ColorDifferenceArgs {
+  color: RgbColor;
+  percent: Percent;
+  colorDifferenceUtility: (color: RgbValue, percent: Percent) => RgbValue;
+}
+
+/**
+ * Internal function for calculating tint or shade.
+ * Should not be used directly in library.
+ *
+ * @param {RgbColor} color - RGB Color object
+ * @param {Percent} percent - number in range 0 - 100
+ * @param {function} colorDifferenceUtility - an utility function which calculate a RGB Color values
+ *
+ * @returns {RgbColor} A color's tint or shade depending on colorDifferenceUtility
+ * */
+export default function getColorDifference({color, percent, colorDifferenceUtility}: ColorDifferenceArgs): RgbColor {
   return {
     red: colorDifferenceUtility(color.red, percent),
     green: colorDifferenceUtility(color.green, percent),
